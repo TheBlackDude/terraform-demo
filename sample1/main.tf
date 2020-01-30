967ea20569e832cc03109ad93a0bfd37b5c248f1
Ousmane Seidy Diallo <ousmane.mudu@gmail.com>
2020-01-30 20:52:58 +0000

    feat: add autoscaling and load balancing

--- sample1/main.tf
+++ sample1/main.tf
@@ -35,15 +35,17 @@ variable "server_port" {
   default     = 8080
 }
 
-/* output "public_ip" {
-  value       = aws_launch_configuration.first_sample.public_ip
-  description = "The public IP of the web server"
-  sensitive   = true
-} */
+output "alb_dns_name" {
+  value       = aws_lb.lb_sample.dns_name
+  description = "The domain name of the load balancer"
+}
 
 resource "aws_autoscaling_group" "first_sample" {
   launch_configuration = aws_launch_configuration.first_sample.name
   vpc_zone_identifier  = data.aws_subnet_ids.subnet_source_sample.ids
+
+  target_group_arns = [aws_lb_target_group.target_sample.arn]
+  health_check_type = "ELB"
 
   min_size = 2
   max_size = 10
@@ -67,6 +69,7 @@ resource "aws_lb" "lb_sample" {
   name               = "terraform-lb-example"
   load_balancer_type = "application"
   subnets            = data.aws_subnet_ids.subnet_source_sample.ids
+  security_groups    = [aws_security_group.lb_security_group_sample.id]
 }
 
 resource "aws_lb_listener" "http" {
@@ -85,5 +88,58 @@ resource "aws_lb_listener" "http" {
     }
   }
 }
+
+resource "aws_security_group" "lb_security_group_sample" {
+  name = "terraform-example-lb-security-group"
+
+  # Allow inbound HTTP requests
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "target_sample" {
+  name     = "terraform-example-target-group"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.vpc_source_sample.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "listener_rule_sample" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    field = "path-pattern"
+    values = ["*"]
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_sample.arn
+  }
+}
+
 
 
